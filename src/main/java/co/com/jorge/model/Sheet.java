cd4927ea5ca0ced6208c3ee1b08858e92aedb00e
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2022-11-12 18:06:49 -0500

    Documentación de la clase Sheet

--- src/main/java/co/com/jorge/model/Sheet.java
+++ src/main/java/co/com/jorge/model/Sheet.java
@@ -5,23 +5,48 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
-import java.util.Objects;
+/**
+ * Clase Sheet, representa cada cuadrícula dentro del tablero, en la cual va almacenada la imagen y responde al evento
+ * del clic para mostrar u ocultar las imágenes
+ */
 
 public class Sheet {
-    private int id;
+    /**
+     * Identificador único de cada objeto de la clase Sheet
+     */
+    private final int id;
 
+    /**
+     * Contenedor de la clase ImageView de javaFX para almacenar las imágenes y mostrarlas en la interfaz gráfica
+     */
     private ImageView image;
 
+    /**
+     * Representación booleana para identificar la imagen está cubierta
+     */
     private boolean cover;
 
+    /**
+     * Representación booleana para identificar que la imagen está descubierta al igual que su semejante
+     */
     private boolean pair;
 
+    /**
+     * Generador de ID único para cada objeto de la clase Sheet
+     */
     public static int generateId = 0;
 
+    /**
+     * Constructor vacío de la clase Sheet
+     */
     public Sheet() {
         this.id = generateId++;
     }
 
+    /**
+     * Constructor con parámetro image de  la clase ImageView
+     * @param image Imagen a cargar en el objeto de la clase Sheet
+     */
     public Sheet(ImageView image) {
         this.id = generateId++;
         this.image = image;
@@ -29,6 +54,10 @@ public class Sheet {
         this.image.addEventHandler(MouseEvent.MOUSE_CLICKED, setHandler());
     }
 
+    /**
+     * Cubre la imagen del objeto de la clase Sheet si este no está identificado como par, lo cual indica que no se ha
+     * encontrado su semejante
+     */
     public void coverImage(){
         if(!isPair()){
             image.setImage(new Image (getClass().getResourceAsStream("/img/img0.jpg")));
@@ -36,11 +65,18 @@ public class Sheet {
         }
     }
 
+    /**
+     * Muestra o descubre la imagen del objeto de la clase Sheet
+     */
     public void uncoverImage(){
         image.setImage(new Image (getClass().getResourceAsStream("/img/" + image.getId() + ".jpg")));
         this.cover = false;
     }
 
+    /**
+     * @return Retorna un evento para el clic del mouse que invoca a los métodos de cubrir o descubrir imagen según
+     * el estado de la imagen
+     */
     private EventHandler<MouseEvent> setHandler() {
         return  (MouseEvent event) -> {
             if (isCover()) {
@@ -51,46 +87,72 @@ public class Sheet {
         };
     }
 
+    /**
+     * @return Retorna el objeto de la clase ImageView contenido en el objeto de la clase Sheet el cual contiene
+     * la imagen
+     */
     public ImageView getImage() {
         return image;
     }
 
+    /**
+     * @param image Carga un objeto de la clase ImageView en el atributo image del objeto de la clase Sheet
+     */
     public void setImage(ImageView image) {
         this.image = image;
     }
 
+    /**
+     * @return Retorna el ID unico del objeto de la clase Sheet
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @return retorna true si la imagen se encuentra cubierta y false si está se encuentra descubierta
+     */
     public boolean isCover() {
         return cover;
     }
 
+    /**
+     * Modifica el estado del objeto de la clase Sheet que representa si la imagen está cubierta o descubierta
+     * @param cover indica el estado que se le dara al objeto de la clase Sheet para representar si la imagen está
+     *              cubierta o descubierta
+     */
     public void setCover(boolean cover) {
         image.setImage(new Image (getClass().getResourceAsStream("/img/img0.jpg")));
-        this.cover = cover;
+        this.cover = true;
     }
 
+    /**
+     * @return Retorna el estado del objeto de la clase Sheet, que representa si la imagen semejante ha sido descubierta
+     */
     public boolean isPair() {
         return pair;
     }
 
+    /**
+     * Modifica el estado del objeto de la clase Sheet, que representa si la imagen semejante ha sido descubierta
+     * @param pair Representa el estado del objeto de la clase Sheet, que representa si la imagen semejante ha
+     *             sido descubierta
+     */
     public void setPair(boolean pair) {
         this.pair = pair;
     }
 
+    /**
+     * @param o Compara el objeto de la clase Sheet con otro objeto de la misma clase para determinar si poseen
+     *          imágenes semejantes
+     * @return retorna true si ambos objetos contiene la misma imagen y false de no ser asi
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Sheet sheet = (Sheet) o;
         return cover == sheet.cover && image.getId().equals(sheet.image.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(image, cover);
     }
 
     @Override
